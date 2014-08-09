65fb425c9f8c2e357b2562ffe6f456dbd327c00e
DSRoden <Daniel.S.Roden@gmail.com>
2014-08-08 23:50:25 -0500

    addTrans

--- app/models/account.js
+++ app/models/account.js
@@ -6,11 +6,11 @@ var _     = require('lodash');
 function Account(o){
   this.name           = o.name;
   this.type           = o.type;
-  this.balance        = parseInt(o.balance);
+  this.balance        = parseFloat(o.balance);
   this.opened         = new Date(o.opened);
   this.photo          = o.photo;
   this.color          = o.color;
-  this.pin            = parseInt(o.pin);
+  this.pin            = o.pin;
   this.transactions   = [];
   this.transferId     = Mongo.ObjectID(o.transferId);
 }
@@ -19,13 +19,51 @@ Object.defineProperty(Account, 'collection', {
   get: function(){return global.mongodb.collection('accounts');}
 });
 
-Account.create = function(o, cb){
-  var a = new Account(o);
-  Account.collection.save(a, cb);
+Account.prototype.insert = function(cb){
+  Account.collection.save(this, cb);
+};
+
+Account.prototype.addTrans = function(trans, cb){
+  if(this.pin === trans.pin){
+  var amt = parseFloat(trans.amt);
+ 
+  this.balance = (trans.isDeposit) ? (this.balance + amt): this.balance;
+  this.balance = (trans.isDraw && this.balance > amt) ? (this.balance - amt) : this.balance;
+ 
+  if( trans.isDraw === true && this.balance < amt){
+     trans.fee = 50;
+     this.balance = this.balance - amt - 50;
+  }
+
+  this.transactions.push(trans);
+  }else { this.balance = this.balance}
+  cb((this.balance).toFixed(2));
+  
 };
 
 Account.all = function(cb){
-  Account.collection.find().toArray(cb);
+  Account.collection.find().toArray(function(err, objects){
+    var accounts = objects.map(function(o){
+     return changePrototype(o);
+    });
+  
+  cb(accounts);
+  });
 };
 
+Account.findById = function(id, cb){
+  var _id = Mongo.ObjectID(id);
+
+  Account.collection.findOne({_id:_id}, function(err, obj){
+    var account = changePrototype(obj);
+
+    cb(account);
+  });
+};
 module.exports = Account;
+
+//Private Function
+
+function changePrototype(obj){
+  return _.create(Account.prototype, obj);
+}
--- test/unit/account.js
+++ test/unit/account.js
@@ -7,8 +7,9 @@ var expect       = require('chai').expect;
 var Account      = require('../../app/models/account');
 var dbConnect    = require('../../app/lib/mongodb');
 var Mongo        = require('mongodb');
-var cp           = require('child_process');
+//var cp           = require('child_process');
 var db           = 'bankr-test';
+var a1,a2,a3;
 
 describe('Account', function(){
   before(function(done){
@@ -20,55 +21,113 @@ describe('Account', function(){
   beforeEach(function(done){
     Account.collection.remove(function(){
    // cp.execFile(__dirname + '/../scripts/freshdb.sh', [db], {cwd:__dirname + '/../scripts'}, function(){
-      done();
+    var o   = {'name':'bill smith', 'type':'savings', 'balance':'100.00', 'opened':'05/08/2011', 'photo':'p1.jpg', 'color':'red', 'pin':'0000', 'transferId': '53d01ddf4fbbd6de0b530013', 'transactions':[]};
+    var o2  = {'name':'sara jones', 'type':'checking', 'balance':'200.00', 'opened':'05/09/2011', 'photo':'p2.jpg', 'color':'blue', 'pin':'1111', 'transferId': '53d01ddf4fbbd6de0b530012', 'transactions':[]};
+    var o3  = {'name':'joe lee', 'type':'checking', 'balance':'300.00', 'opened':'05/10/2011', 'photo':'p3.jpg', 'color':'orange', 'pin':'2222', 'trnasferId':'53d01ddf4fbbd6de0b530011', 'transactions':[]};
+     a1  = new Account(o);
+     a2  = new Account(o2);
+     a3  = new Account(o3);
+      a1.insert(function(){
+        a2.insert(function(){
+          a3.insert(function(){
+            done();
+          });
+        });
+      });  
     });
   });
 
   describe('constructor', function(){
     it('should create a new Account object', function(){
-      var o = {name:'jane doe', type: 'checking', balance: '50.00', opened:'05/07/2011', photo: 'p.jpg', color: 'purple', pin: '3333', transferId:'53d01ddf4fbbd6de0b530014'};
-      var a1 = new Account(o);
-      expect(a1).to.be.instanceof(Account);
-      expect(a1.name).to.equal('jane doe');
-      expect(a1.type).to.equal('checking');
-      expect(a1.balance).to.equal(50.00);
-      expect(a1.opened).to.be.instanceof(Date);
-      expect(a1.photo).to.equal('p.jpg');
-      expect(a1.color).to.equal('purple');
-      expect(a1.pin).to.equal(3333);
-      expect(a1.transactions).to.have.length(0);
-      expect(a1.transferId).to.be.instanceof(Mongo.ObjectID);
+      var o4 = {name:'jane doe', type: 'checking', balance: '50.00', opened:'05/07/2011', photo: 'p.jpg', color: 'purple', pin: '3333', transferId:'53d01ddf4fbbd6de0b530014'};
+      var a4 = new Account(o4);
+      expect(a4).to.be.instanceof(Account);
+      expect(a4.name).to.equal('jane doe');
+      expect(a4.type).to.equal('checking');
+      expect(a4.balance).to.equal(50.00);
+      expect(a4.opened).to.be.instanceof(Date);
+      expect(a4.photo).to.equal('p.jpg');
+      expect(a4.color).to.equal('purple');
+      expect(a4.pin).to.equal('3333');
+      expect(a4.transactions).to.have.length(0);
+      expect(a4.transferId).to.be.instanceof(Mongo.ObjectID);
     });
   });
 
-  describe('.create', function(){
+  describe('.insert', function(){
     it('should create and save a new Account object', function(done){
-      Account.create({name:'jane doe', type: 'checking', balance: '50.00', opened:'05/07/2011', photo: 'p.jpg', color: 'purple', pin: '3333', transferId:'53d01ddf4fbbd6de0b530014'}, function(err, account){
-        expect(account._id).to.be.instanceof(Mongo.ObjectID);
-        expect(account).to.be.instanceof(Account);
-        expect(account.name).to.equal('jane doe');
-        expect(account.type).to.equal('checking');
-        expect(account.balance).to.equal(50.00);
-        expect(account.opened).to.be.instanceof(Date);
-        expect(account.photo).to.equal('p.jpg');
-        expect(account.color).to.equal('purple');
-        expect(account.pin).to.equal(3333);
-        expect(account.transactions).to.have.length(0);
-        expect(account.transferId).to.be.instanceof(Mongo.ObjectID);
+      var o4 = {name:'jane doe', type: 'checking', balance: '50.00', opened:'05/07/2011', photo: 'p.jpg', color: 'purple', pin: '3333', transferId:'53d01ddf4fbbd6de0b530014'};
+      var a4 = new Account(o4);
+      a4.insert(function(){
+        expect(a4._id).to.be.instanceof(Mongo.ObjectID);
+        expect(a4).to.be.instanceof(Account);
+        expect(a4.name).to.equal('jane doe');
+        expect(a4.type).to.equal('checking');
+        expect(a4.balance).to.equal(50.00);
+        expect(a4.opened).to.be.instanceof(Date);
+        expect(a4.photo).to.equal('p.jpg');
+        expect(a4.color).to.equal('purple');
+        expect(a4.pin).to.equal('3333');
+        expect(a4.transactions).to.have.length(0);
+        expect(a4.transferId).to.be.instanceof(Mongo.ObjectID);
         done(); 
       });
     });
   });
 
+  describe('#addTrans', function(){
+    it('should increase the balance of an account', function(done){
+    var t = {pin: '0000', amt: '100.00', isDeposit: true, isDraw: false};
+      a1.addTrans(t, function(){
+        expect(a1.balance).to.equal(200.00);
+        done();
+      });
+    });
+
+    it('should decrease the balance of an account', function(done){
+    var t1 = {pin: '1111', amt: '100.00', isDeposit: false, isDraw: true};
+      a2.addTrans(t1, function(){
+        expect(a2.balance).to.equal(100.00);
+        done();
+      });
+    });
+
+    it('should make no change to the balance of an account', function(done){
+    var t2 = {pin: '5697', amt: '100.00', isDeposit: true, isDraw: false};
+      a3.addTrans(t2, function(){
+        expect(a3.balance).to.equal(300.00);
+        done();
+      });
+    });
+
+    it('should add a transaction fee for overdraft', function(done){
+    var t3 = {pin: '2222', amt: '400.00', isDeposit: false, isDraw: true};
+      a3.addTrans(t3, function(){
+        expect(a3.balance).to.equal(-150.00);
+        expect(t3.fee).to.equal(50.00);
+        done();
+      });
+    });
+  });  
+
   describe('.all', function(){
     it('should get all accounts from database', function(done){
-      Account.all(function(err, accounts){
-        expect(accounts).to.have.length(1);
+      Account.all(function(accounts){
+        expect(accounts).to.have.length(3);
         done();
       });
     });
   }); 
-
+  
+  describe('.findById', function(){
+    it('should find an account by its id', function(done){
+      Account.findById(a1._id.toString(), function(account){
+        expect(account.name).to.equal('bill smith');
+        expect(account).to.be.instanceof(Account);
+        done();
+       });
+    });
+  });
 //last brace  
 });
 
